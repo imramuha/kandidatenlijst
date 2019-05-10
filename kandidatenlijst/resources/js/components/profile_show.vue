52810c0c3a21937858999c2c3d56ecd5c1a454cc
jensrott <jensrott@student.arteveldehs.be>
2019-05-10 14:48:42 +0200

    frontend

---
+++ kandidatenlijst/resources/js/components/profile_show.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        A specific profile
+        <router-link to="/">Home</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
