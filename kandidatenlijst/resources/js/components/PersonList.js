e54895a43b1dd54a03b76007ce61961d16f4c3e5
jensrott <jensrott@student.arteveldehs.be>
2019-05-14 11:03:29 +0200

    push

--- kandidatenlijst/resources/js/components/PersonList.js
+++ kandidatenlijst/resources/js/components/PersonList.js
@@ -24,6 +24,7 @@ class PersonList extends React.Component {
           <ul>
             <li>{person.Name}</li>
             <li>{person.Email}</li>
+            <li></li>
           </ul>)}
       </ul>
     )
