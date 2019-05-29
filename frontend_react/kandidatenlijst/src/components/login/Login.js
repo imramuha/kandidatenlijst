6857ae3b69677ab36243e3ecb6f93d48667fc7ee
Imran <imramuha@student.arteveldehs.be>
2019-05-29 14:43:21 +0200

    changes to routing

--- frontend_react/kandidatenlijst/src/components/login/Login.js
+++ frontend_react/kandidatenlijst/src/components/login/Login.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 import helpers from '../../helpers';
 
-// Deze tutorial volgen: https://www.youtube.com/watch?v=Nq9RmAB9eag
-// Werkt nog niet volledig
-
 class AuthComponent extends Component {
   constructor(props) {
     super(props);
